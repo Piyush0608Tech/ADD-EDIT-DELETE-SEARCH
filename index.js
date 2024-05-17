104545b7674e0c88f15b9fa150c901148853ff5c
Piyush0608Tech <piyushpathade2@gmail.com>
2024-05-17 09:57:19 +0530

    learned

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 const form = document.querySelector("form");
 const fruits = document.querySelector(".fruits");
-console.log("Form submitted");
+
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    const fruitadd= document.getElementById("fruit-to-add");
+    const fruittoadd = document.getElementById("fruit-to-add");
     const newli = document.createElement("li");
-    const newliText = document.createTextNode(fruitadd.value);
-    newli.appendChild(newliText);
-    const delt = document.createElement("button");
-    const delttext = document.createTextNode("x");
-    delt.appendChild(delttext);
-    delt.className="delete-btn";
-    newli.appendChild(delt);
+    const newlitext = document.createTextNode(fruittoadd.value);
+    newli.appendChild(newlitext);
+    const del = document.createElement("button");
+    const deltext = document.createTextNode("x");
+    del.appendChild(deltext);
+    del.className="delete-btn"
+    newli.appendChild(del);
     const edit = document.createElement("button");
-    const edittext = document.createTextNode("Edit");
+    const edittext = document.createTextNode("edit");
     edit.appendChild(edittext);
-    edit.className="edit-btn";
+    edit.className="edit-btn"
     newli.appendChild(edit);
     fruits.appendChild(newli);
-     
 })
 
 fruits.addEventListener("click", function(event){
-    if ( event.target.classList.contains("delete-btn")){
-        const del = event.target.parentElement;
-        fruits.removeChild(del);
+    if(event.target.classList.contains("delete-btn")){
+        const frudel = event.target.parentElement;
+        fruits.removeChild(frudel);
     }
 })
 
-// filter 
-
 const filter = document.getElementById("filter");
 filter.addEventListener("keyup", function(event){
-const textEntered = event.target.value.toLowerCase();
-const fi = document.getElementsByClassName("fruit");
-for (let i = 0; i< fi.length; i++){
-    const cf = fi[i].firstChild.textContent.toLowerCase();
-    if(cf.indexOf(textEntered) === -1){
-       fi[i].style.display="none"
+    const textentered = event.target.value.toLowerCase();
+    const fi = document.getElementsByClassName("fruit");
+    for (let  i= 0;  i< fi.length; i++) {
+        const cf = fi[i].firstChild.textContent.toLowerCase();
+        if(cf.indexOf(textentered)=== -1){
+            fi[i].style.display="none";
+        }
+        
     }
 
-}
-});
+})
